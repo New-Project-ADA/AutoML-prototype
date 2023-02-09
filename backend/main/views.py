91072f65a6263f2d648a535b02bdb7cc430b2855
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2023-02-09 20:42:11 +0700

    remove auth

--- backend/main/views.py
+++ backend/main/views.py
@@ -4,6 +4,8 @@ from .serializers import MainSerializer, DataSerializer
 from .models import Task, DataInput
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -16,8 +18,11 @@ class DataInput(viewsets.ModelViewSet):
     queryset = DataInput.objects.order_by('-id')
     serializer_class = DataSerializer
     parser_classes = (MultiPartParser, FormParser)
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save()
+        
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
