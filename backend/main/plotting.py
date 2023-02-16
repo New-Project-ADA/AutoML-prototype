069eced2b19f6afe1e7f6ed7516062175b977cbd
Dahlan Yasa <mdahlany@Gmail.com>
2023-02-16 16:06:45 +0700

    update get data for plotting

--- backend/main/plotting.py
+++ backend/main/plotting.py
@@ -11,10 +11,13 @@ from shapely.geometry import Point, Polygon
 from .models import DataInput
 from .views import *
 from django.conf import settings
+from sklearn.metrics import confusion_matrix, f1_score
 
-def load_data(filename,AREA,date_start=None,date_end=None):
+def load_data(df,df_m,df_b,AREA,date_start=None,date_end=None):
     path = settings.MEDIA_ROOT
-    df = pd.read_csv(path + '/data/{filename}'.format(filename=filename)).drop('Unnamed: 0',1)
+    '''df = pd.read_csv(path + '/data/{filename}'.format(filename=filename)).drop('Unnamed: 0',1)
+    df_m = pd.read_csv(f'./katalog/{FOLDER}m{CAVE}.csv').drop('Unnamed: 0',1)
+    df_b = pd.read_csv(f'./katalog/{FOLDER}b{CAVE}.csv').drop('Unnamed: 0',1)'''
     df = df[df['bound']!='0']
     df = df[df['v3']>=0]
     minmag = min(df['v5'].min(), -3)
@@ -37,8 +40,8 @@ def load_data(filename,AREA,date_start=None,date_end=None):
         df_b = df_b[df_b['datetime']>=date_start][df_b['datetime']<=date_end]
     return df.sort_values('datetime').reset_index(drop=True), df_m.sort_values('datetime').reset_index(drop=True), df_b.sort_values('datetime').reset_index(drop=True), minmag, maxmag, Xmax, Xmin, Ymax, Ymin, Zmax, Zmin
 
-def generate_features(CAVE,AREA,Zmax=None,Zmin=None,c_true=True,b_true=True,m_true=True):
-    df, df_m, df_b, minmag, maxmag, Xmax, Xmin, Ymax, Ymin, Zmax, Zmin = load_data(CAVE,AREA)
+def generate_features(df,df_m,df_b,AREA,Zmax=None,Zmin=None,c_true=True,b_true=True,m_true=True):
+    df, df_m, df_b, minmag, maxmag, Xmax, Xmin, Ymax, Ymin, Zmax, Zmin = load_data(df,df_m,df_b,AREA)
     df['date'] = df['datetime'].apply(lambda x: x.date())
     df_m['date'] = df_m['datetime'].apply(lambda x: x.date())
     df_b['date'] = df_b['datetime'].apply(lambda x: x.date())
@@ -80,9 +83,13 @@ def generate_features(CAVE,AREA,Zmax=None,Zmin=None,c_true=True,b_true=True,m_tr
     df_series = pd.DataFrame(index=pd.date_range(dt0,dt1)).join(df_stat, how='left').fillna(0)
     return df_series
 
-def data_for_plot(filename):
-    df_series = generate_features(filename,'',c_true=True,b_true=True,m_true=True)
+def data_for_plot(df,df_m,df_b):
+    df_series = generate_features(df,df_m,df_b,'',c_true=True,b_true=True,m_true=True)
     return df_series
+
+#------------------------------------------------
+#get data untuk plot
+#------------------------------------------------
 
 def df_corr_plot(series):
     df_features = series.reset_index(drop=True) 
@@ -113,3 +120,45 @@ def statistic_features(CAVE,AREA,date_start=None,date_end=None,Zmax=None,Zmin=No
             stats = [df.describe(), df_b.describe()]
     print(len(stats))
     return stats
+
+def get_data_plot_fitur(series,target_date,fitur,window=30):
+    X = series.loc[:target_date].iloc[-30:]
+    print(X[fitur])
+    return X[fitur]
+
+def get_data_plot_risk(series,target_date,tnoutput=55, get_score=True):
+    X = series.loc[target_date:].iloc[:tnoutput]
+    index = X.index
+    randomvalues = np.random.uniform(0,1,[3,tnoutput])
+    data = {
+        'actual': [min(max((_)//1,0),3) for _ in X['v5|max'].values],
+        'predicted': [min(max((_)//1,0),3) for _ in X['v5|max'].values+randomvalues.mean(0)],
+    }
+    return index, data
+
+def get_data_confusion_matrix(series,target_date,tnoutput=55, get_score=True):
+    X = series.loc[target_date:].iloc[:tnoutput]
+    index = X.index
+    randomvalues = np.random.uniform(0,1,[3,tnoutput])
+    data = {
+        'actual': [min(max((_)//1,0),3) for _ in X['v5|max'].values],
+        'predicted': [min(max((_)//1,0),3) for _ in X['v5|max'].values+randomvalues.mean(0)],
+    }
+    next7day = int(np.random.uniform(0,5)//1)
+    cm = confusion_matrix(data['actual'],data['predicted'], labels = [0,1,2,3])
+    accuracy = np.trace(cm) / np.sum(cm).astype('float')
+
+    return cm, round(accuracy,6), next7day
+
+def plot_uncertainty(series,target_date,tnoutput=7):
+    plt.figure(figsize=(12,6))
+    X = series.loc[target_date:].iloc[:tnoutput]
+    index = X.index
+    randomvalues = np.random.uniform(-1,1,[3,tnoutput])
+    data = {
+        'actual': X['v5|max'].values,
+        'median': X['v5|max'].values+randomvalues.mean(0),
+        'lower': X['v5|max'].values+randomvalues.min(0),
+        'upper': X['v5|max'].values+randomvalues.max(0),
+    }
+    return index, data
